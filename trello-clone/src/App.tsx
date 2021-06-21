8379a2d58c850d19a611d4d0989a9980a568da56
Khorally Pierre <kvpierre@gmail.com>
2021-06-20 21:19:26 -0400

    react typescript practice

---
+++ trello-clone/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {AppContainer } from "./styles"
+import {FC} from "react"
+import {Column} from "./Column"
+import { Card } from "./Card"
+import { AppContainer} from "./styles"
+
+
+export const App FC = ({children}) => {
+    return (
+      <AppContainer>
+        Column will go here
+      </AppContainer>
+    )
+}
+
+
+
+export default App;
